8ba3c8e5e90ae8369a9ef2e1f5ca68f549f78487
artur <w.artur.kucherenko@gmail.com>
2025-02-05 20:48:49 +0200

    Initial commit

---
+++ Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Test;
+
+class Program
+{
+    static void Main(
+        string[] args)
+    {
+        var list = new List<int> { 1, 2, 3 };
+        var readonlyCollection = new ReadOnlyCollection<int>(list);
+        var readonlyList = list as IReadOnlyList<int>;
+        var ilist = readonlyCollection as IList<int>;
+        
+        ilist.Add(2);
+    }
+}
